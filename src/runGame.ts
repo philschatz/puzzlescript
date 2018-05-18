c588ff238c630ad3ee73c3ca16fc19196af8ab04
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-05-18 03:26:30 -0500

    add game code coverage

--- src/runGame.ts
+++ src/runGame.ts
@@ -1,4 +1,5 @@
-import {readFileSync} from 'fs'
+import {readFileSync, writeFileSync} from 'fs'
+import * as path from 'path'
 import * as glob from 'glob'
 import * as pify from 'pify'
 
@@ -42,13 +43,87 @@ async function run () {
 
         UI.renderScreen(data, engine.currentLevel)
 
+        // record the changes in a coverage.json file
+        const codeCoverageTemp = {} // key = Line number, value = count of times the rule executed
+
+        // First add all the Tiles, Legend Items, collisionLayers, Rules, and Levels.
+        // Then, after running, add all the matched rules.
+        function coverageKey(node) {
+          // the HTML reporter does not like multiline fields. Rather than report multiple times, we just report the 1st line
+          // This is a problem with `startloop`
+          const {start, end} = node.__getLineAndColumnRange()
+          if (start.line !== end.line) {
+            return JSON.stringify({ start , end: { line: start.line, col: start.col + 3}})
+          }
+          return JSON.stringify({ start, end })
+        }
+        function addNodeToCoverage(node) {
+          codeCoverageTemp[coverageKey(node)] = 0
+        }
+        // data.objects.forEach(addNodeToCoverage)
+        // data.legends.forEach(addNodeToCoverage)
+        // data.sounds.forEach(addNodeToCoverage)
+        // data.collisionLayers.forEach(addNodeToCoverage) // these entries are sometimes (always?) null
+        data.rules.forEach(addNodeToCoverage)
+        data.winConditions.forEach(addNodeToCoverage)
+        // data.levels.forEach(addNodeToCoverage)
+
+
         for (var i = 0; i < 10; i++) {
           await sleep(500)
           const changes = engine.tick()
-          if (changes.length === 0) {
+          if (changes.size === 0) {
             break
           }
+
+          // record the tick coverage
+          for (const [rule, cellsCovered] of changes.entries()) {
+            const line = coverageKey(rule)
+            if (!codeCoverageTemp[line]) {
+              codeCoverageTemp[line] = 0
+            }
+            codeCoverageTemp[line] = codeCoverageTemp[line] + 1
+          }
         }
+
+        const codeCoverage2 = Object.entries(codeCoverageTemp).map(([key, value]) => {
+          return {loc: JSON.parse(key), count: value}
+        })
+        // Generate the coverage.json file from which Rules were applied
+        const statementMap = {}
+        const fnMap = {}
+        const f = {}
+        const s = {}
+
+        // Add all the matched rules
+        codeCoverage2.forEach(({loc, count}, index) => {
+
+          s[index] = count
+          statementMap[index] = loc
+          f[index] = count
+          fnMap[index] = {
+            name: "foo",
+            decl: loc
+            loc: loc,
+            line: loc.start.line
+          }
+
+        })
+
+        const absPath = path.resolve(filename)
+        const gistName = path.basename(path.dirname(filename))
+        const codeCoverageEntry = {
+          path: absPath,
+          s: s,
+          statementMap: statementMap,
+          fnMap: fnMap,
+          f: f,
+          branchMap: {},
+          b: {}
+        }
+        const codeCoverageObj = {}
+        codeCoverageObj[absPath] = codeCoverageEntry
+        writeFileSync(`coverage/coverage-gists-${gistName}.json`, JSON.stringify(codeCoverageObj, null, 2)) // indent by 2 chars
       }
 
       UI.clearScreen()
