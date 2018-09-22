7c0e4cc11487f869fb7388d6973de3c9506cbbe5
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-09-22 13:00:05 -0500

    minify the rule names
    so the JS file size drops from 770k to 206k

--- test/parseFilesUnique.test.js
+++ test/parseFilesUnique.test.js
@@ -4,6 +4,8 @@ const path = require('path')
 const { default: Parser } = require('../lib/parser/parser')
 
 const GISTS_ROOT = path.join(__dirname, '../gists/')
+const GIST_SOLUTIONS_ROOT = path.join(__dirname, '../gist-solutions/')
+
 
 describe('parsing files unambiguously', () => {
 
@@ -14,12 +16,16 @@ describe('parsing files unambiguously', () => {
         const gistDirs = fs.readdirSync(GISTS_ROOT)
         // it('checks all files that they parse uniquely', () => {
         gistDirs.forEach(gistDirName => {
-            const codeFile = path.join(GISTS_ROOT, gistDirName, 'script.txt')
-            if (fs.existsSync(codeFile)) {
-                it (`parses ${gistDirName} uniquely`, () => {
-                    const code = fs.readFileSync(codeFile, 'utf-8')
-                    Parser.parse(code)
-                })
+            // Only parse files that do not have solutions
+            // because solutions will be tested by running the games
+            if (!fs.existsSync(path.join(GIST_SOLUTIONS_ROOT, `${gistDirName}.json`))) {
+                const codeFile = path.join(GISTS_ROOT, gistDirName, 'script.txt')
+                if (fs.existsSync(codeFile)) {
+                    it (`parses ${gistDirName} uniquely`, () => {
+                        const code = fs.readFileSync(codeFile, 'utf-8')
+                        Parser.parse(code)
+                    })
+                }
             }
         })
         // })
