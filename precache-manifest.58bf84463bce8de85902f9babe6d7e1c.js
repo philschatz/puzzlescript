5799631bbb493ea6427ab254e8d8976ee25ef53b
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-01-18 02:40:53 -0600

    compile

---
+++ precache-manifest.58bf84463bce8de85902f9babe6d7e1c.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "b880ee836914dab5bcc3",
+    "url": "puzzlescript.js"
+  },
+  {
+    "revision": "d5e8be295fe4d3b7b9eaf720bac66576",
+    "url": "index.xhtml"
+  }
+];
