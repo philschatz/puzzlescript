6418f726c5ee8e60f6225675ec1e18b3bb4f39e7
Phil Schatz <253202+philschatz@users.noreply.github.com>
2020-08-12 00:09:20 -0500

    massive package update

--- typings/inquirer-autocomplete-prompt.d.ts
+++ typings/inquirer-autocomplete-prompt.d.ts
@@ -3,9 +3,9 @@ declare module 'inquirer-autocomplete-prompt' {
 
     // This is hacky and I don't know what it does but I got rid of compiler errors.
 
-    import {PromptModule} from 'inquirer'
+    import Inquirer from 'inquirer'
 
-    const x: PromptModule
+    const x: Inquirer.prompts.PromptConstructor
     export = x;
 }
 
