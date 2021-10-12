80f4f099dfa70cdb0173a902bd9122cc0e295948
philschatz <253202+philschatz@users.noreply.github.com>
2021-10-12 12:19:46 -0500

    add some ignores to trigger version bump

--- packages/example/index.js
+++ packages/example/index.js
@@ -4,13 +4,13 @@ const gameSourceString = require('puzzlescript/games/entanglement-one/script.txt
 
 const table = document.querySelector('table') // selector to the <table> that will be used
 const optionalEventHandler = {
+    // onMessage:      (...args) => console.log('[async onMessage]', ...args),
+    // onSound:        (...args) => console.log('[async onSound]', ...args),
     onGameChange:   (...args) => console.log('[onGameChange]', ...args),
     onPress:        (...args) => console.log('[onPress]', ...args),
-    // onMessage:      (...args) => console.log('[async onMessage]', ...args),
     onLevelLoad:    (...args) => console.log('[onLevelLoad]', ...args),
     onLevelChange:  (...args) => console.log('[onLevelChange]', ...args),
     onWin:          (...args) => console.log('[onWin]', ...args),
-    // onSound:        (...args) => console.log('[async onSound]', ...args),
     onTick:         (...args) => console.log('[onTick]', ...args),
     onPause:        (...args) => console.log('[onPause]', ...args),
     onResume:       (...args) => console.log('[onResume]', ...args),
